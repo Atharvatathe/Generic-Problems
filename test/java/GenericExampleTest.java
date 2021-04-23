5e46232eb4df4948ae45d5f681506d265c853179
Atharvatathe <atharva.tathe55@gmail.com>
2021-04-23 16:25:44 +0530

    Added: Passing Maximum float Number Third and return same

--- test/java/GenericExampleTest.java
+++ test/java/GenericExampleTest.java
@@ -24,4 +24,24 @@ public class GenericExampleTest {
         Assertions.assertEquals(5,max);
     }
 
+    @Test
+    public void givenFloatNumber_FirstMaximum_returnMaximumNumber(){
+        GenericExample generic = new GenericExample();
+        Float max = generic.findMaximumFloat(7.8f,4.4f,6.6f);
+        Assertions.assertEquals(7.8f,max);
+    }
+
+    @Test
+    public void givenFloatNumber_SecondMaximum_returnMaximumNumber() {
+        GenericExample generic = new GenericExample();
+        Float max = generic.findMaximumFloat(4.4f, 7.8f, 6.6f);
+        Assertions.assertEquals(7.8f, max);
+    }
+
+    @Test
+    public void givenFloatNumber_ThirdMaximum_returnMaximumNumber() {
+        GenericExample generic = new GenericExample();
+        Float max = generic.findMaximumFloat(4.4f, 6.6f, 7.8f);
+        Assertions.assertEquals(7.8f, max);
+    }
 }
