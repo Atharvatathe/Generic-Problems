9863260fe82440a0ebe21c31604f8d80540c4ae9
Atharvatathe <atharva.tathe55@gmail.com>
2021-04-23 16:16:03 +0530

    Added: Welcome to Generic Examples

--- GenericExample.java
+++ GenericExample.java
@@ -1,6 +1,6 @@
 public class GenericExample{
 	public static void main(String[] args){
 		
-		System.out.println("Welcome to Generic Examples");
+		System.out.println("Welcome to Generic Examples ");
 	}
 }
