82375abbd26607303d9fd1608790f2d89f549b8f
Atharvatathe <atharva.tathe55@gmail.com>
2021-04-23 13:37:57 +0530

    Added: Passing Maximum Number second and return same

--- test/java/GenericExampleTest.java
+++ test/java/GenericExampleTest.java
@@ -9,4 +9,11 @@ public class GenericExampleTest {
         Integer max = generic.findMaximumInt(5,4,3);
         Assertions.assertEquals(5,max);
     }
+
+    @Test
+    public void givenNumber_SecondMaximum_returnMaximumNumber(){
+        GenericExample generic = new GenericExample();
+        Integer max = generic.findMaximumInt(4,5,3);
+        Assertions.assertEquals(5,max);
+    }
 }
