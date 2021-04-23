a2152ca7dafaf2ef4ab901ce49dbd7070763f765
Atharvatathe <atharva.tathe55@gmail.com>
2021-04-23 16:02:28 +0530

    Added: find Maximum between 3 Float object

--- main/java/GenericExample.java
+++ main/java/GenericExample.java
@@ -7,13 +7,30 @@ public class GenericExample {
             max = b;
         if (c.compareTo(max) == 1)
             max = c;
-        printMaximum(max);
+        printIntMaximum(max);
         return max;
     }
 
-    public static void printMaximum(Integer m) {
-        System.out.println("Maximum Number is: " + m);
+    public static void printIntMaximum(Integer m) {
+        System.out.println("Maximum integer Number is: " + m);
     }
 
+    public static Float findMaximumFloat(Float a, Float b, Float c) {
+        Float max;
+        max = a;
+        if (b.compareTo(max) == 1)
+            max = b;
+        if (c.compareTo(max) == 1)
+            max = c;
+        printFloatMaximum(max);
+        return max;
+    }
+    public static void printFloatMaximum(Float m) {
+        System.out.println("Maximum float Number is: " + m);
+    }
 
+public static void main(String[] args){
+        GenericExample g = new GenericExample();
+        g.findMaximumFloat(6.4f,2.2f,4.5f);
+}
 }
