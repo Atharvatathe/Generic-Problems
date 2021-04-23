e5a359d35208b4445934650c584c520223545a6e
Atharvatathe <atharva.tathe55@gmail.com>
2021-04-23 13:05:33 +0530

    Added: find Maximum between 3 Integer object

---
+++ main/java/GenericExample.java
@@ -0,0 +1,20 @@
+public class GenericExample {
+    private static  Integer max;
+    public static Integer findMaximum(Integer a,Integer b,Integer c){
+        max = a;
+        if(b.compareTo(max) == 1)
+            max = b;
+        if(c.compareTo(max) == 1)
+            max = c;
+        return max;
+    }
+
+    public static void main(String [] args){
+
+        GenericExample generic = new GenericExample();
+        Integer max = generic.findMaximum(4,3,2);
+        System.out.println("Maximum Number is: " +max);
+    }
+
+
+}
